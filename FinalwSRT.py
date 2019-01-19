516900c21bf1075d0cab732d0955e9a16fe9432e
PieButNotThatPie <>
2019-01-19 15:34:54 +0700

    Update FinalwSRT.py
    Delete second and similar directory creation on the srt stage

--- FinalwSRT.py
+++ FinalwSRT.py
@@ -28,9 +28,7 @@ for path in Path(src_dir).glob("**/*.srt"):
         sname, snum = data.groups()
         # make the dst folder
         dst = os.path.join(dst_dir, sname, "Season "+snum)
-        os.makedirs(dst, exist_ok=True)
         shutil.copy(path, dst)
     else:
         dst1 = os.path.join(dst1_dir, path.stem)
-        os.makedirs(dst1, exist_ok=True)
         shutil.copy(path, dst1)
